3b5abb468d8a18c3b9e57087a999a387d2f9b0a5
bilalcaliskan <bilalcaliskan@protonmail.com>
2024-01-07 23:38:43 +0300

    fix: fix linter warnings

--- cmd/foo/foo.go
+++ cmd/foo/foo.go
@@ -5,26 +5,24 @@ import (
 
 	rootopts "github.com/bilalcaliskan/split-the-tunnel/cmd/root/options"
 
-	"github.com/bilalcaliskan/split-the-tunnel/internal/logging"
-	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	logger = logging.GetLogger()
+	//logger = logging.GetLogger()
 	fooOpts = options.GetFooOptions()
 	fooOpts.InitFlags(FooCmd)
 }
 
 var (
-	logger  zerolog.Logger
+	//logger  zerolog.Logger
 	fooOpts *options.FooOptions
 	// FooCmd represents the bar command
 	FooCmd = &cobra.Command{
 		Use:   "foo",
 		Short: "",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			logger = cmd.Context().Value(rootopts.LoggerKey{}).(zerolog.Logger)
+			//logger = cmd.Context().Value(rootopts.LoggerKey{}).(zerolog.Logger)
 			rootOpts := cmd.Context().Value(rootopts.OptsKey{}).(*rootopts.RootOptions)
 			fooOpts.RootOptions = rootOpts
 
